44e47bd1c9692914cb7e9e9528dc1ea56c360601
Omar Atef <20190356@stud.fci-cu.edu.eg>
2024-06-30 11:42:11 +0300

    Created the models

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,6 +77,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #TODO : Uncomment the below code to use MySQL before submitting!!
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'LittleLemon',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # }
 }
 
 # Password validation
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    number_of_guests = models.PositiveIntegerField()
+    booking_date = models.DateTimeField()
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
