259c7ba950b4e0b4a5d9ab3294a6335c8e28b4d2
Omar Atef <20190356@stud.fci-cu.edu.eg>
2024-06-30 12:56:58 +0300

    Booking API and Menu API Created!

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('booking/tables/', views.BookingView.as_view({"get": "list","post":"create"}), name='booking'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
 ]
